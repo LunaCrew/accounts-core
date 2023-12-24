94981f97418ab0e4ca1df7ae5df3508f51ec8f8a
Ash Trindade <ashtrindade5@gmail.com>
2023-12-23 21:12:49 -0300

    ref: move connection to app.ts

--- test/unit/config/dbConnect.spec.ts
+++
@@ -1,8 +0,0 @@
-import connectDatabase from 'src/config/dbConnect'
-
-describe.only(':: Config :: DbConnect ::', () => {
-  it('should call connectDatabase()', () => {
-    const message = connectDatabase()
-    expect(message).toHaveBeenCalled
-  })
-})
