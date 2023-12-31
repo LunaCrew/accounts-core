d9fa7779e741575535102338720293b905e7d327
Ash Trindade <ashtrindade5@gmail.com>
2023-12-31 16:29:17 -0300

    feat: enum custom error

---
+++ src/util/enum/CustomError.ts
@@ -0,0 +1,6 @@
+enum CustomError {
+  PASSWORD = '"password" must have uppercase and lowercase letters, numbers and special characters',
+  DATE_TIME_TIMEZONE = '"dateTime" must be in ISO format <YYYY-MM-DD>T<HH:mm:ss>-<HH:mm>'
+}
+
+export default CustomError
