7a898b9026dbc32a7e891879dbf40d3e46d6f8e0
Ash Trindade <ashtrindade5@gmail.com>
2024-01-02 18:18:37 -0300

    feat: update enum

--- test/unit/src/util/enum/HttpStatusCode.spec.ts
+++ test/unit/src/util/enum/HttpStatusCode.spec.ts
@@ -3,7 +3,7 @@ import HttpStatusCode from 'src/util/enum/HttpStatusCode'
 describe(':: Util :: Enum :: HttpStatusCode ::', () => {
   it('should return expected keys', () => {
     const enumKeys = Object.keys(HttpStatusCode)
-    const keys = ['OK', 'CREATED', 'NO_CONTENT', 'BAD_REQUEST', 'UNAUTHORIZED', 'FORBIDDEN', 'NOT_FOUND', 'INTERNAL_SERVER_ERROR', 'BAD_GATEWAY', 'SERVICE_UNAVAILABLE', 'CONNECTION_TIMED_OUT']
+    const keys = ['OK', 'CREATED', 'NO_CONTENT', 'BAD_REQUEST', 'UNAUTHORIZED', 'FORBIDDEN', 'NOT_FOUND', 'CONFLICT', 'INTERNAL_SERVER_ERROR', 'BAD_GATEWAY', 'SERVICE_UNAVAILABLE', 'CONNECTION_TIMED_OUT']
 
     enumKeys.forEach((key, index) => {
       expect(key).toBe(keys[index])
@@ -12,7 +12,7 @@ describe(':: Util :: Enum :: HttpStatusCode ::', () => {
 
   it('should return expected values', () => {
     const enumValues = Object.values(HttpStatusCode)
-    const values = ['200', '201', '204', '400', '401', '403', '404', '500', '502', '503', '522']
+    const values = ['200', '201', '204', '400', '401', '403', '404', '409', '500', '502', '503', '522']
 
     enumValues.forEach((value, index) => {
       expect(value).toBe(values[index])
