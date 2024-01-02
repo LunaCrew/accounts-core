52a226881b19c205752615be9dc1bcc60653082a
Ash Trindade <ashtrindade5@gmail.com>
2024-01-02 18:19:01 -0300

    fix: adjust logger

--- src/util/log/Logger.ts
+++ src/util/log/Logger.ts
@@ -3,26 +3,26 @@ import colors from 'colors'
 
 class Logger {
   static success = (message: string) => {
-    console.log(colors.bgGreen.bold(message), '\n')
+    console.log(colors.green.bold(message), '\n')
   }
 
   static info = (message: string) => {
-    console.log(colors.bgBlue.bold(message), '\n')
+    console.log(colors.blue.bold(message), '\n')
   }
 
   static warning = (message: string) => {
-    console.log(colors.bgYellow.bold(message), '\n')
+    console.log(colors.yellow.bold(message), '\n')
   }
 
   static error = (message: string, error: any) => {
-    const log = {
-      error: error.name,
-      description: error.message,
-      details: error.stack
+    const logError = {
+      name: error.name,
+      message: error.message,
+      stack: error.stack
     }
 
-    console.log(colors.bgRed.bold(message), '\n')
-    console.log(log)
+    console.log(colors.red.bold(message), '\n')
+    throw logError
   }
 }
 
