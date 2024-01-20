324a998cd918c07c7237cc741d25af1b300e8614
Ash Trindade <ashtrindade5@gmail.com>
2024-01-20 14:30:58 -0300

    feat: router

---
+++ src/router/routes.ts
@@ -0,0 +1,16 @@
+import express, { Application } from 'express'
+import userRouter from './userRouter'
+import Logger from 'src/util/log/Logger'
+
+const routes = (app: Application) => {
+  try {
+    app.use(
+      express.json(),
+      userRouter
+    )
+  } catch (error) {
+    Logger.error(':: Router :: Routes ::', error)
+  }
+}
+
+export { routes }
