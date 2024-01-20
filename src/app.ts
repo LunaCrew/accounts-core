a65ac7e26137b01342b45dd5b3d3da33b8bd55c4
Ash Trindade <ashtrindade5@gmail.com>
2024-01-20 14:30:36 -0300

    ref: change mongoose to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,45 @@
-import { connect } from 'mongoose'
-import express, { Request, Response } from 'express'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import express from 'express'
 import Logger from './util/log/Logger'
-import HttpStatusCode from './util/enum/HttpStatusCode'
-import routes from './router/routes'
+import { routes } from './router/routes'
 import * as dotenv from 'dotenv'
 dotenv.config({ path: '.env' })
 
 const PORT = process.env.PORT || 3000
 const app: express.Application = express()
 
-try {
-  app.get('/', (_req: Request, res: Response) => {
-    res.status(parseInt(HttpStatusCode.OK)).json({
-      server: 'Luna App',
-      status: 'OK'
-    })
-  })
-} catch (error) {
-  Logger.error(':: App :: Base URL ::', error)
-}
-
 routes(app)
 
-try {
-  app.listen(PORT, () => {
-    Logger.success(`:: Server running on port ${PORT} ::`)
-  })
-} catch (error) {
-  Logger.error(':: App :: Start server ::', error)
+export const client = new MongoClient(process.env.DB_URI || '', {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
+
+async function start(): Promise<void> {
+  try {
+    app.listen(PORT, () => {
+      Logger.success(`:: Server running on port ${PORT} ::`)
+    })
+  } catch (error) {
+    Logger.error(':: App :: Start server ::', error)
+  }
 }
 
-try {
-  connect(process.env.DB_URI || '', { dbName: 'lunateam-dev' })
-  Logger.success(':: Database connected with success ::')
-} catch (error) {
-  Logger.error(':: Config :: DbConnect ::', error)
+async function connect(): Promise<void> {
+  try {
+    await client.connect()
+    await client.db().command({ ping: 1 })
+    Logger.success(':: Connected to MongoDB! ::')
+  } catch (error) {
+    await client.close()
+    Logger.error(':: Database Connection ::', error)
+  }
 }
+
+export const usersCollection = client.db().collection('users')
+
+start()
+connect()
