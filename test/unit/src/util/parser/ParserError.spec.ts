f92ec32746c06803db756fd78f55d6225063dbec
Ash Trindade <ashtrindade5@gmail.com>
2024-01-20 14:28:59 -0300

    fix: parserError test

--- test/unit/src/util/parser/ParserError.spec.ts
+++ test/unit/src/util/parser/ParserError.spec.ts
@@ -3,15 +3,25 @@ import HttpStatusCode from 'src/util/enum/HttpStatusCode'
 
 describe(':: Util :: Log :: ParserError ::', () => {
   it('should parse a http error', () => {
-    const code = HttpStatusCode.BAD_REQUEST
-    const message = 'Bad request error'
     const expected = {
-      code: parseInt(code),
-      error: Object.keys(HttpStatusCode)[Object.values(HttpStatusCode).indexOf(code)],
-      details: message
+      code: 400,
+      error: 'BAD_REQUEST',
+      details: 'Bad request error'
     }
 
-    const result = ParserError.http(code, message)
+    const result = ParserError.http(HttpStatusCode.BAD_REQUEST, 'Bad request error')
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should parse a http error', () => {
+    const expected = {
+      code: 400,
+      error: 'BAD_REQUEST',
+      details: ''
+    }
+
+    const result = ParserError.http(HttpStatusCode.BAD_REQUEST)
 
     expect(result).toEqual(expected)
   })
