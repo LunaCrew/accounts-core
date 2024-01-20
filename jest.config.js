019d38bae4e251aaa04d60aab82da0cb3abce132
Ash Trindade <ashtrindade5@gmail.com>
2024-01-20 14:31:35 -0300

    test: show missing files on coverage

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   modulePathIgnorePatterns: ['build'],
   coverageReporters: ['clover', 'json-summary', 'lcov', ['text', { skipFull: true }]],
   coveragePathIgnorePatterns: ['src/app.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
   rootDir: '.',
   modulePaths: ['<rootDir>']
 };
