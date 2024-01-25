c5dcd883eb128ee6e24a0ce43b314a5bea615ad7
Ash Trindade <ashtrindade5@gmail.com>
2024-01-25 19:20:02 -0300

    feat: get user by params

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -5,10 +5,11 @@ import userSchema from '../schema/userSchema'
 import HttpStatusCode from '../util/enum/HttpStatusCode'
 import Password from '../util/security/Password'
 import ParserError from '../util/parser/ParserError'
+import { userParams } from '../schema/userSchema'
 import Logger from '../util/log/Logger'
 
 export default class UserController {
-  public static createUser = async (req: Request, res: Response): Promise<void> => {
+  public static createUser = (req: Request, res: Response) => {
     const { error, value } = userSchema.validate(req.body)
     const user = value
 
@@ -21,16 +22,66 @@ export default class UserController {
     user.password = Password.encrypt(req.body.password)
 
     try {
-      await usersCollection.insertOne(user).then((result) => {
+      usersCollection.insertOne(user).then((result) => {
         if (result) {
           res.status(parseInt(HttpStatusCode.CREATED)).json(result)
         } else {
-          res.status(parseInt(HttpStatusCode.INTERNAL_SERVER_ERROR)).json(ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'internal error'))
+          res.status(parseInt(HttpStatusCode.INTERNAL_SERVER_ERROR)).json(
+            ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'internal error')
+          )
         }
       })
       Logger.info(':: Calling Endpoint :: CreateUser ::')
     } catch (error) {
       Logger.error(':: Controller :: UserController :: CreateUser ::', error)
+      res.status(parseInt(HttpStatusCode.INTERNAL_SERVER_ERROR)).json(
+        ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'An error occurred')
+      )
+    }
+  }
+
+  public static getUser = (req: Request, res: Response) => {
+    const params = {
+      id: req.query.id,
+      email: req.query.email,
+      username: req.query.username
+    }
+    
+    
+    const { error } = userParams.validate(params)
+    if (error) {
+      res.status(parseInt(HttpStatusCode.BAD_REQUEST)).json({ error: error.details })
+      return
+    }
+    
+    try {
+      const query: { $and: Array<object> } = { $and: [] }
+      if (params.id) query.$and.push({ _id: params.id })
+      if (params.email) query.$and.push({ email: params.email })
+      if (params.username) query.$and.push({ username: params.username })
+
+      if (query.$and.length === 0) {
+        res.status(parseInt(HttpStatusCode.BAD_REQUEST)).json(
+          ParserError.http(HttpStatusCode.BAD_REQUEST, 'id, email or username is required')
+        )
+        return
+      }
+
+      usersCollection.findOne(query, { projection: { password: 0 } }).then((result) => {
+        if (result) {
+          res.status(parseInt(HttpStatusCode.OK)).json(result)
+        } else {
+          res.status(parseInt(HttpStatusCode.NOT_FOUND)).json(
+            ParserError.http(HttpStatusCode.NOT_FOUND, 'not found')
+          )
+        }
+      })
+      Logger.info(':: Calling Endpoint :: GetUser ::')
+    } catch (error) {
+      Logger.error(':: Controller :: UserController :: GetUserById ::', error)
+      res.status(parseInt(HttpStatusCode.INTERNAL_SERVER_ERROR)).json(
+        ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'An error occurred')
+      )
     }
   }
 }
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -4,5 +4,6 @@ import UserController from '../controller/UserController'
 const userRouter = express.Router()
 
 userRouter.post('/api/user', UserController.createUser, userRouter)
+userRouter.get('/api/user', UserController.getUser, userRouter)
 
 export default userRouter
