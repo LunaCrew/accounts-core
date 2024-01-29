e55b9716ed6ce94e3349ad80724e00e48da5ce00
Ash Trindade <ashtrindade5@gmail.com>
2024-01-29 18:53:58 -0300

    ref: change enum values type

--- test/unit/src/util/enum/HttpStatusCode.spec.ts
+++ test/unit/src/util/enum/HttpStatusCode.spec.ts
@@ -1,21 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import HttpStatusCode from 'src/util/enum/HttpStatusCode'
 
 describe(':: Util :: Enum :: HttpStatusCode ::', () => {
-  it('should return expected keys', () => {
-    const enumKeys = Object.keys(HttpStatusCode)
-    const keys = ['OK', 'CREATED', 'NO_CONTENT', 'BAD_REQUEST', 'UNAUTHORIZED', 'FORBIDDEN', 'NOT_FOUND', 'CONFLICT', 'INTERNAL_SERVER_ERROR', 'BAD_GATEWAY', 'SERVICE_UNAVAILABLE', 'CONNECTION_TIMED_OUT']
+  it('should have all the keys', () => {
+    const expectedKeys = [
+      'OK',
+      'CREATED',
+      'NO_CONTENT',
+      'BAD_REQUEST',
+      'UNAUTHORIZED',
+      'FORBIDDEN',
+      'NOT_FOUND',
+      'CONFLICT',
+      'INTERNAL_SERVER_ERROR',
+      'BAD_GATEWAY',
+      'SERVICE_UNAVAILABLE',
+      'CONNECTION_TIMED_OUT'
+    ]
 
-    enumKeys.forEach((key, index) => {
-      expect(key).toBe(keys[index])
+    const enumKeysDeclared = Object.keys(HttpStatusCode).filter((key: any) => isNaN(key))
+
+    expect(expectedKeys.length).toEqual(enumKeysDeclared.length)
+    expectedKeys.forEach((key, index) => {
+      expect(key).toEqual(enumKeysDeclared[index])
     })
   })
 
-  it('should return expected values', () => {
-    const enumValues = Object.values(HttpStatusCode)
-    const values = ['200', '201', '204', '400', '401', '403', '404', '409', '500', '502', '503', '522']
+  it('should have all the values', () => {
+    const expectedValues = [
+      200,
+      201,
+      204,
+      400,
+      401,
+      403,
+      404,
+      409,
+      500,
+      502,
+      503,
+      522
+    ]
 
-    enumValues.forEach((value, index) => {
-      expect(value).toBe(values[index])
+    const enumValuesDeclared = Object.values(HttpStatusCode).filter((key: any) => !isNaN(key))
+    expect(expectedValues.length).toEqual(enumValuesDeclared.length)
+    expectedValues.forEach((value, index) => {
+      expect(value).toEqual(enumValuesDeclared[index])
     })
   })
 })
