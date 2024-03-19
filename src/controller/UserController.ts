2c31052811776a906fdf0d76864aa5ca51f6462e
Ash Trindade <ashtrindade5@gmail.com>
2024-03-18 21:32:22 -0300

    ref: adjust user controller

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,54 +1,48 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { collections } from '../app'
-import HttpStatusCode from '../util/enum/HttpStatusCode'
-import ParserError from '../util/parser/ParserError'
+import { NotFound } from '../error/CustomError'
+import { UserService } from '../types/Service'
 import CreateUserService from '../service/CreateUserService'
 import GetUserService from '../service/GetUserService'
+import CustomErrorMessage from '../util/enum/CustomErrorMessage'
+import HttpStatus from '../util/enum/HttpStatus'
 import Logger from '../util/log/Logger'
 
 export default class UserController {
-  public static createUser = async (req: Request, res: Response) => {
+  public static readonly createUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const user = CreateUserService(req, res)
+      const user: UserService = CreateUserService.execute(req, next)
       if (!user) return
 
-      const result = await collections.users.insertOne(user)
+      const result = await collections.users.insertOne(user).catch((error) => {
+        next(error)
+      })
 
       if (result) {
-        res.status(HttpStatusCode.CREATED).json({ id: result.insertedId })
-      } else {
-        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(
-          ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'internal error')
-        )
-        Logger.error(':: Controller :: UserController :: CreateUser ::', result)
+        res.status(HttpStatus.code.CREATED).json({ id: result.insertedId })
       }
       Logger.info(':: Calling Endpoint :: CreateUser ::')
     } catch (error) {
-      Logger.error(':: Controller :: UserController :: CreateUser ::', error)
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(
-        ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'An error occurred')
-      )
+      Logger.error(':: Controller :: UserController :: CreateUser ::', `${error}`)
     }
   }
 
-  public static getUser = async (req: Request, res: Response) => {
+  public static readonly getUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const query = new GetUserService(req, res)
+      const query: UserService = GetUserService.execute(req, next)
+      if (!query) return
+
       const result = await collections.users.findOne(query, { projection: { password: 0 } })
 
       if (result) {
-        res.status(HttpStatusCode.OK).json(result)
+        res.status(HttpStatus.code.OK).json(result)
       } else {
-        res.status(HttpStatusCode.NOT_FOUND).json(
-          ParserError.http(HttpStatusCode.NOT_FOUND, 'not found')
-        )
+        next(new NotFound(CustomErrorMessage.NOT_FOUND))
+        next()
       }
       Logger.info(':: Calling Endpoint :: GetUser ::')
     } catch (error) {
-      Logger.error(':: Controller :: UserController :: GetUser ::', error)
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(
-        ParserError.http(HttpStatusCode.INTERNAL_SERVER_ERROR, 'An error occurred')
-      )
+      Logger.error(':: Controller :: UserController :: GetUser ::', `${error}`)
     }
   }
 }
