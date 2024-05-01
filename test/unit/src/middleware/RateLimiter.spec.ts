1f5681a3c23471a7ea7e1ae7b75352c726155a52
Ash Trindade <ashtrindade5@gmail.com>
2024-04-30 21:46:39 -0300

    Fix rate limiter test in RateLimiter.spec.ts

--- test/unit/src/middleware/RateLimiter.spec.ts
+++ test/unit/src/middleware/RateLimiter.spec.ts
@@ -5,11 +5,14 @@ jest.mock('express-rate-limit')
 
 describe('RateLimiter middleware', () => {
   it('should call express-rate-limit with the correct configuration', () => {
-    rateLimiter
     expect(rateLimit).toHaveBeenCalledWith({
       windowMs: 60 * 1000 * 10,
       max: 10,
       message: 'Too many requests, please try again later.'
     })
+  })
+
+  it('should fail', () => {
+    expect(rateLimiter).toBeUndefined()
   })
 })
