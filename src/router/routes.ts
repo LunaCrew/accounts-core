448ef499396099e2a709999fb2e4d4b2929f2f01
Ash Trindade <ashtrindade5@gmail.com>
2024-05-14 21:39:55 -0300

    Refactor routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,16 @@
-import express, { Application } from 'express'
-import userRouter from './userRouter'
+import express, { Application, Router } from 'express'
+import UserController from '../controller/UserController'
+import rateLimiter from '../middleware/RateLimiter'
+
+const userRouter: Router = Router()
 
 const routes = (app: Application) => {
-  app.use(
-    express.json(),
-    userRouter
-  )
+  app.use(express.json())
+
+  app
+    .post('/api/user', rateLimiter, UserController.createUser, userRouter)
+    .get('/api/user', rateLimiter, UserController.getUser, userRouter)
+    .delete('/api/user/:id', rateLimiter, UserController.deleteUser, userRouter)
 }
 
 export { routes }
