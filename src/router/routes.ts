3c322fedf7b299c8f6dbde7dfa7aa2e881431398
Ash Trindade <ashtrindade5@gmail.com>
2024-05-14 21:57:45 -0300

    Refactor routes and add API documentation for user endpoints

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,16 +1,49 @@
-import express, { Application, Router } from 'express'
+import express, { Application, Router, Request, Response } from 'express'
+import passport from 'passport'
 import UserController from '../controller/UserController'
 import rateLimiter from '../middleware/RateLimiter'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerFile from '../../docs/swagger.json'
 
 const userRouter: Router = Router()
 
 const routes = (app: Application) => {
-  app.use(express.json())
+  app
+    .use(express.json())
+    .use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
   app
-    .post('/api/user', rateLimiter, UserController.createUser, userRouter)
-    .get('/api/user', rateLimiter, UserController.getUser, userRouter)
-    .delete('/api/user/:id', rateLimiter, UserController.deleteUser, userRouter)
+    .get('/', (_req: Request, res: Response) => {
+      res.send({ status: 'API is OK!', docs: '/api/docs' })
+    })
+
+    .post(
+      '/api/user',
+      rateLimiter,
+      UserController.createUser,
+      userRouter
+    )
+
+    .get(
+      '/api/user',
+      passport.authenticate('jwt', { session: false }),
+      UserController.getUser,
+      userRouter
+    )
+
+    .delete(
+      '/api/user/:id',
+      passport.authenticate('jwt', { session: false }),
+      UserController.deleteUser,
+      userRouter
+    )
+
+    .post(
+      '/api/auth/login',
+      rateLimiter,
+      UserController.login,
+      userRouter
+    )
 }
 
 export { routes }
