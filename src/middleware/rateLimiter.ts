6ef488d4031fa74d8f1e0eb6b6b3e54d7ea01383
Ash Trindade <ashtrindade5@gmail.com>
2024-06-01 14:25:53 -0300

    Adjust unit test

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit'
 
 const rateLimiter = rateLimit({
-  windowMs: 60 * 1000 * 10,
+  windowMs: 60 * 1000 * 5,
   max: 10,
   message: 'Too many requests, please try again later.'
 })
