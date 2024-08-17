b283cb6ce3ec70ed9e09a3d4165c25c4a51e4386
Ash Trindade <ashtrindade5@gmail.com>
2024-08-17 17:46:04 -0300

    chore: rename Service.spec.ts to Query.spec.ts

--- test/unit/src/types/Query.spec.ts
+++ test/unit/src/types/Query.spec.ts
@@ -1,18 +1,18 @@
-import { UserService } from 'src/types/Service'
+import { GeneralUserQuery } from 'src/types/Query'
 
-describe('UserService', () => {
+describe('GeneralUserQuery', () => {
   it('should allow object type', () => {
-    const user: UserService = { name: 'John', age: 30 }
+    const user: GeneralUserQuery = { name: 'John', age: 30 }
     expect(user).toMatchObject({ name: 'John', age: 30 })
   })
 
   it('should allow null type', () => {
-    const user: UserService = null
+    const user: GeneralUserQuery = null
     expect(user).toBeNull()
   })
 
   it('should allow undefined type', () => {
-    const user: UserService = undefined
+    const user: GeneralUserQuery = undefined
     expect(user).toBeUndefined()
   })
 })
