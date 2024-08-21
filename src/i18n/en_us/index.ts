5ea4b45c0b57a66ca27202276ffca8414faf0d99
Ash Trindade <ashtrindade5@gmail.com>
2024-08-21 18:29:23 -0300

    chore: install i18n

---
+++ src/i18n/en_us/index.ts
@@ -0,0 +1,7 @@
+import type { BaseTranslation } from '../i18n-types'
+
+const en_us = {
+  HI: 'Hi {name:string}!',
+} satisfies BaseTranslation
+
+export default en_us
---
+++ src/i18n/pt_br/index.ts
@@ -0,0 +1,7 @@
+import type { Translation } from '../i18n-types'
+
+const pt_br = {
+  HI: 'Olá {name}!',
+} satisfies Translation
+
+export default pt_br
