7072b08f35ccb2c58e9a067162dddb9f825bf480
Ash Trindade <ashtrindade5@gmail.com>
2024-08-26 22:19:23 -0300

    chore: delete unused enum

--- src/util/log/Log.ts
+++ src/util/log/Log.ts
@@ -1,12 +1,11 @@
 import { Tag } from '../../types/Log'
-import EnumColor from '../enum/Color'
 import formatTimestamp from './timestamp'
 
 export default class Log {
   private static readonly _formattedTimestamp = formatTimestamp(new Date())
   private static readonly _timestamp = new Date().toISOString()
-  private static readonly _cyan = EnumColor.cyan
-  private static readonly _base = EnumColor.base
+  private static readonly _cyan = '\x1b[36m' // #00FFFF
+  private static readonly _base = '\x1b[0m' // default color
 
   public static readonly debug = (tag: Tag, message: string): void => {
     const log = `${this._cyan}[ ${this._formattedTimestamp} ] ${this._base}=> ${message}\n`
